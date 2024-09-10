b26171e4eec670656e7192c4b29f6ad9cf5e84fa
Mohd Anas <mohdanas.dev70@gmail.com>
2024-09-10 16:41:36 +0530

    contextApi proj1 complete

--- 08minicontext/src/main.jsx
+++ 08minicontext/src/main.jsx
@@ -8,3 +8,9 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+
+// 1) Pehle context create krte hai 
+// 2)Component contextProvider ka banana hota hai usme context provider name se tag banate hai usme acces hota hai children ka, or provider ko btana hota hai k kaunsi value children access krega
+// 3)contextProvider me jo children hota hai wo components hote hai jo value access krte provider se.
+// 4)value sent ya receive krne k liye useContext() hool use hota hai jisme pass krna hota hai contextprovider js file ,yaha contextProvder js file k through contextProvider jsx file se value manupulate hoti hai jo ki us k children access krte hai. 
